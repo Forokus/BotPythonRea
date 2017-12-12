b60a1822985fa348409d3f065ca9556f2fe89b20
Forokus <33478835+Forokus@users.noreply.github.com>
2017-12-12 16:30:17 +0300

    Update Json.py

--- Json.py
+++ Json.py
@@ -1,8 +1,8 @@
 import json
 
 def encode_json(name, dictionary):
-    with open('{}.json'.format(name),'w') as file:
-        json.dump(dictionary, file, indent=2, ensure_ascii=False)
+	with open('{}.json'.format(name),'w') as file:
+        	json.dump(dictionary, file, indent=2, ensure_ascii=False)
 	
 def decode_json(name):
 	with open('{}.json'.format(name)) as file:
